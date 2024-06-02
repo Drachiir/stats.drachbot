9b4b0585006da5e48b97a4cda3b17da705a43c25
Drachir <gaming.drachir@gmail.com>
2024-06-02 11:43:37 +0200

    changes

--- util.py
+++ util.py
@@ -150,15 +150,15 @@ def time_ago(time=False):
         if second_diff < 10:
             return "just now"
         if second_diff < 60:
-            return str(second_diff) + " seconds ago"
+            return str(round(second_diff)) + " seconds ago"
         if second_diff < 120:
             return  "a minute ago"
         if second_diff < 3600:
-            return str( second_diff / 60 ) + " minutes ago"
+            return str( round(second_diff / 60) ) + " minutes ago"
         if second_diff < 7200:
             return "an hour ago"
         if second_diff < 86400:
-            return str( second_diff / 3600 ) + " hours ago"
+            return str( round(second_diff / 3600) ) + " hours ago"
     if day_diff == 1:
         return "Yesterday"
     if day_diff < 7:
