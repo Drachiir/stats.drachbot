8dc9275953e48294b8ecbc2d6a5c9181fdac2104
Drachir <gaming.drachir@gmail.com>
2024-07-12 11:36:59 +0200

    fix

--- app.py
+++ app.py
@@ -183,7 +183,7 @@ def stats(stats, elo, patch, specific_key):
             header_title = "Roll"
             header_cdn = "https://cdn.legiontd2.com/icons/"
             if specific_key == "All":
-                header_keys = ["Games", "Winrate", "Usage Rate", "Player Elo"]
+                header_keys = ["Games", "Winrate", "Pickrate", "Player Elo"]
                 sub_headers = [["Best Combo", "ComboUnit", "rollstats"], ["Best MMs", "MMs", "mmstats"], ["Best Spell", "Spells", "spellstats"]]
             else:
                 header_keys = ["Games", "Winrate", "Playrate"]
