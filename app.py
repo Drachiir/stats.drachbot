58fb42f95a1f0ae8b7c7a573a4c938daa23b78ee
Drachir <gaming.drachir@gmail.com>
2024-10-05 08:26:32 +0200

    fix for v11 profile patch

--- app.py
+++ app.py
@@ -212,6 +212,9 @@ def profile(playername, stats, patch, elo, specific_key):
         new_patch = request.args.get('patch')
         if new_patch:
             patch = new_patch.replace("/", "")
+            if "11" in patch.split(","):
+                patch = "11"
+                new_patch = "11"
             new_patch = f"?patch={new_patch}"
         else:
             new_patch = ""
