e8b6820eca0c9c2a21facd2e030b7781e4e88bc1
Drachir <gaming.drachir@gmail.com>
2024-10-12 19:27:55 +0200

    x

--- drachbot/peewee_pg.py
+++ drachbot/peewee_pg.py
@@ -24,7 +24,7 @@ with open("Files/json/Secrets.json", "r") as f:
 
 db = PooledPostgresqlExtDatabase(
     "postgres",
-    max_connections=50,
+    max_connections=150,
     stale_timeout=60,
     server_side_cursors=True,
     user=secret_file["pg_user"],
